2e4ab228232a901b0ea63077717a4285c7b33e59
priya4212 <nspriya805@gmail.com>
2016-09-01 12:05:43 +0530

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,27 @@
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		for(int i='A';i<'Z';i++)
+		{
+			System.out.println(i);
+		}
+		System.out.println();
+		for(int i='a';i<'z';i++)
+		{
+			System.out.println(i);
+		}
+		System.out.println();
+		for(int i=0;i<=9;i++)
+		{
+			System.out.println(i+48);
+		}
+		System.out.println();
+		for(int i=0;i<15;i++)
+		{
+			System.out.println(i+33);
+		}
+		
+	}
+
+}
