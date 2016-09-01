b1aa03dbd91ce956fdc8bdea54372271a5355d31
priya4212 <nspriya805@gmail.com>
2016-09-01 12:15:09 +0530

    Create Removevowels.java

---
+++ Removevowels.java
@@ -0,0 +1,18 @@
+
+public class Removevowels {
+
+	public static void main(String[] args) {
+	String str="reverse";
+	for(int i=0;i<str.length();i++)
+	{
+		if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u')
+		{
+        }
+		else
+		{
+			System.out.println(str.charAt(i));
+		}
+	}
+	}
+
+}
